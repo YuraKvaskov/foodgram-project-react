73c1a34d616f34a482eda5caf594edd89ac98cd3
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-05-25 16:09:51 +0300

    на повторное ревью

--- backend/api/Serializers.py
+++ backend/api/Serializers.py
@@ -103,7 +103,7 @@ class IngredientsEditSerializer(serializers.ModelSerializer):
     amount = serializers.IntegerField()
 
     class Meta:
-        model = Ingredient
+        model = IngredientRecipe
         fields = ('id', 'amount')
 
 
