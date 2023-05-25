52bf3c30cb367554f4439b031c8ed982a29d78b9
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-05-25 08:47:53 +0300

    на повторное ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -158,12 +158,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
             raise PermissionDenied('Пользователь не авторизован.')
         serializer.save(author=self.request.user)
 
-    def get_permissions(self):
-        if self.action in ['delete', 'partial_update']:
-            permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
+    # def get_permissions(self):
+    #     if self.action in ['delete', 'partial_update']:
+    #         permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    #     else:
+    #         permission_classes = [IsAuthenticated]
+    #     return [permission() for permission in permission_classes]
 
     def favorites(self, request, *args, **kwargs):
         queryset = Favorite.objects.filter(user=self.request.user)
