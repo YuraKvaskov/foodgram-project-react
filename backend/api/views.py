7d47d46e6026454a7d33fdfc7f7cee0a40a96e8d
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-05-25 11:24:13 +0300

    на ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db.models import Sum
 from django.http import HttpResponse
-# from django_filters import rest_framework as filters
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, filters
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
@@ -12,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from djoser.views import UserViewSet
 
-from api.filters import CustomRecipeFilter
+from api.filters import CustomRecipeFilter, CustomIngredientFilter
 from api.pagination import CustomPagination
 from api.Serializers import (
     TagSerializer,
@@ -130,8 +129,8 @@ class IngredientViewSet(ReadOnlyModelViewSet):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
     permission_classes = (IsAdminOrReadOnly,)
-    # filter_backends = [filters.DjangoFilterBackend]
-    # filterset_class = CustomIngredientFilter
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = CustomIngredientFilter
     pagination_class = None
 
     def get_queryset(self):
