7a97f37c6b21febc88eb3c2ff991996ec411bbed
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-05-25 12:58:22 +0300

    на ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -135,19 +135,12 @@ class IngredientViewSet(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         name = self.request.query_params.get('name')
-        queryset = super().get_queryset()
-
-        if name:
-            name = name.lower()
-            start_queryset = list(queryset.filter(name__istartswith=name))
-            ingridients_set = set(start_queryset)
-            cont_queryset = queryset.filter(name__icontains=name)
-            start_queryset.extend(
-                [ing for ing in cont_queryset if ing not in ingridients_set]
-            )
-            queryset = start_queryset
-
-        return queryset
+        queryset = super().get_queryset().filter(name__icontains=name)
+        start_queryset = list(queryset.filter(name__istartswith=name))
+        return (
+            start_queryset
+            + [ing for ing in queryset if ing not in start_queryset]
+        )
 
 
 class TagViewSet(ReadOnlyModelViewSet):
