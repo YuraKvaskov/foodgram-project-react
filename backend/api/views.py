007746e18d4394660f91723cce9b016a8909d3ef
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-05-25 18:41:53 +0300

    на повторное ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -144,13 +144,7 @@ class IngredientViewSet(ReadOnlyModelViewSet):
             if name[0] == '%':
                 name = unquote(name)
             name = name.lower()
-            start_queryset = list(queryset.filter(name__istartswith=name))
-            ingridients_set = set(start_queryset)
-            cont_queryset = queryset.filter(name__icontains=name)
-            start_queryset.extend(
-                [ing for ing in cont_queryset if ing not in ingridients_set]
-            )
-            queryset = start_queryset
+            queryset = queryset.filter(name__istartswith=name)
         return queryset
 
 
