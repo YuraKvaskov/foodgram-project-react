f4e7f3a00ed8bf277650319166a226fb061703c1
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-05-25 09:47:20 +0300

    на повторное ревью

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,18 +1,18 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Recipe, Tag
 
 User = get_user_model()
 
 
-class CustomIngredientFilter(FilterSet):
-    name_starts_with = filters.CharFilter(
-        field_name='name',
-        lookup_expr='startswith')
-
-    class Meta:
-        model = Ingredient
-        fields = ['name_starts_with']
+# class CustomIngredientFilter(FilterSet):
+#     name_starts_with = filters.CharFilter(
+#         field_name='name',
+#         lookup_expr='startswith')
+#
+#     class Meta:
+#         model = Ingredient
+#         fields = ['name_starts_with']
 
 
 class CustomRecipeFilter(FilterSet):
